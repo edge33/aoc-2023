96944baedcb512b61d66a3b588e31605f48b885f
Francesco <francesco@efem.dev>
2023-12-01 13:09:42 +0100

    day 01

--- src/days/1/Puzzle.ts
+++ src/days/1/Puzzle.ts
@@ -1,15 +1,37 @@
 const first = (input: string) => {
-  console.log(input);
-  return 'solution 1';
+  return input.split('\n').reduce((acc, next) => {
+    const digits = Array.from(next.matchAll(/\d/g));
+    return acc + +`${digits[0]}${digits[digits.length - 1]}`;
+  }, 0);
 };
 
-const expectedFirstSolution = 'solution 1';
+const expectedFirstSolution = 54239;
 
 const second = (input: string) => {
-  console.log(input);
-  return 'solution 2';
+  return input.split('\n').reduce((acc, next) => {
+    const match = Array.from(
+      next.matchAll(/(?=(one|two|three|four|five|six|seven|eight|nine))|\d/g)
+    ).reduce((acc, next) => [...acc, ...next.filter((n) => n)], []);
+    const numberDictionary: { [key: string]: number } = {
+      one: 1,
+      two: 2,
+      three: 3,
+      four: 4,
+      five: 5,
+      six: 6,
+      seven: 7,
+      eight: 8,
+      nine: 9,
+    };
+    return (
+      acc +
+      +`${numberDictionary[match[0]] || match[0]}${
+        numberDictionary[match[match.length - 1]] || match[match.length - 1]
+      }`
+    );
+  }, 0);
 };
 
-const expectedSecondSolution = 'solution 2';
+const expectedSecondSolution = 55343;
 
 export { first, expectedFirstSolution, second, expectedSecondSolution };
